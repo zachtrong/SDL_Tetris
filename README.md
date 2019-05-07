a52a499ec7d364b073a4f9e73ceb2decab419d53
Nguyen Tien Trong <zimportant.christian@gmail.com>
2019-05-07 08:19:11 +0700

    update readme; seperate model class

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Tetris
+
 Another tetris game reconstructed by `zimportant` and `nguyenthanhvinh` for the sake of entertainment :)
 
-## How to build
-scripts/build.bat
+## Building
 
-## How to build then run immediately
-scripts/run.bat
+1. `scripts/run`
+Debugging purpose (build then run immediately)
+2. `scripts/build.bat`
+Build debug application into `build/` directory
+3. `scripts/release.bat`
+Build release application into `release/` directory
+4. `scripts/test.bat`
+Run some basic tests of implementation
 
 ## Note
+
 Project uses smart pointer and various advanced techniques known as design pattern
 So be careful to understand before implement
--- src/GameView.h
+++ src/GameView.h
@@ -15,43 +15,8 @@
 #include "Constants.h"
 #include "PointerDefinition.h"
 #include "Board.h"
+#include "Model.h"
 using namespace std;
-
-struct DisplayObject {
-    string path;
-    SDL_Rect rect;
-    DisplayObject(string _path, const SDL_Rect &_rect): path(_path), rect(_rect) {}
-};
-
-struct RectObject {
-	SDL_Rect rect;
-	SDL_Color color;
-	RectObject(const SDL_Rect &_rect, const SDL_Color &_color): rect(_rect), color(_color) {}
-};
-
-struct FontObject {
-	string text; 
-	int fontSize; 
-	SDL_Rect rect; 
-	bool isBold;
-	FontObject(string _text, const SDL_Rect &_rect): text(_text), fontSize(24), rect(_rect), isBold(false) {} 
-	FontObject(string _text, int _fontSize, const SDL_Rect &_rect): 
-		text(_text), fontSize(_fontSize), rect(_rect), isBold(false) {} 
-	FontObject(string _text, int _fontSize, const SDL_Rect &_rect, bool _isBold): 
-		text(_text), fontSize(_fontSize), rect(_rect), isBold(_isBold) {} 
-};
-
-struct TileObject {
-	TileType type;
-	SDL_Rect rect;
-	TileObject(TileType _type, const SDL_Rect &_rect): type(_type), rect(_rect) {}
-};
-
-struct FullTileObject {
-	TileType type;
-	SDL_Rect rect;
-	FullTileObject(TileType _type, const SDL_Rect &_rect): type(_type), rect(_rect) {}
-};
 
 class GameView {
 private:
